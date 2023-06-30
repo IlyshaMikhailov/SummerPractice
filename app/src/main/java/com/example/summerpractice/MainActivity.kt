ad0afc9beb288727a1fbcb23c88176d3f9594395
ILYSHAMIKHAILOV <mix.il2812@gmail.com>
2023-07-01 00:11:26 +0300

    thirdpush

--- app/src/main/java/com/example/summerpractice/MainActivity.kt
+++ app/src/main/java/com/example/summerpractice/MainActivity.kt
@@ -9,45 +9,50 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var clickButton: Button? = null
-    private lateinit var etname: EditText
-    private lateinit var etheight: EditText
-    private lateinit var etweight: EditText
-    private lateinit var etage: EditText
-    private lateinit var etvalue: TextView
+    private var etname: EditText? = null
+    private var etheight: EditText? = null
+    private var etweight: EditText? = null
+    private var etage: EditText? = null
+    private var etvalue: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         clickButton = findViewById(R.id.button)
-        etname = findViewById(R.id.editTextText3)
-        etheight = findViewById(R.id.editTextText2)
-        etweight = findViewById(R.id.editTextText)
-        etage = findViewById(R.id.editTextText4)
+        etname = findViewById(R.id.nametext)
+        etheight = findViewById(R.id.heighttext)
+        etweight = findViewById(R.id.weighttext)
+        etage = findViewById(R.id.agetext)
         etvalue = findViewById(R.id.textView)
 
         clickButton?.setOnClickListener {
-            val username = etname.text.toString().trim()
-            val weight = etweight.text.toString()
-            val height = etheight.text.toString()
-            val age = etage.text.toString()
-            var value = etvalue.text
+            val username= etname?.text.toString().trim()
+            val weight = etweight?.text.toString()
+            val height = etheight?.text.toString()
+            val age = etage?.text.toString()
+            var value = etvalue?.text
+            var isСorrect = true
             if (username.isEmpty() || username.length > 50) {
-                etname.error = "Имя введено некорректно"
-                return@setOnClickListener
-            } else if (weight./*toString().*/isEmpty() || weight.toDouble() > 250.0 || weight.toDouble() < 0.0) {
-                etweight.error = "Вес введен некорректно!"
-                return@setOnClickListener
-            } else if (height.toString().isEmpty() || height.toInt()> 250 || height.toInt()< 0) {
-                etheight.error = "Рост введен некорректно!"
-                return@setOnClickListener
-            } else if (age.toString().isEmpty() || age.toInt() > 150 || age.toInt()< 0) {
-                etage.error = "Возраст введен некорректно!"
-                return@setOnClickListener
-            } else
+                etname?.error = getString(R.string.nameexception)
+                isСorrect = false
+            }
+            if (weight.isEmpty() || weight.toDouble() > 250.0 || weight.toDouble() < 0.0) {
+                etweight?.error = getString(R.string.weightException)
+                isСorrect = false
+            }
+            if (height.isEmpty() || height.toInt()> 250 || height.toInt()< 0) {
+                etheight?.error = getString(R.string.heightException)
+                isСorrect = false
+            }
+            if (age.isEmpty() || age.toInt() > 150 || age.toInt()< 0) {
+                etage?.error = getString(R.string.ageException)
+                isСorrect = false
+            }
+            if(isСorrect)
                 Toast.makeText(this, "Валидация завершена", Toast.LENGTH_SHORT).show()
             value =
                 (height.toInt() % 10 + weight.toDouble() % 10 + username.length + age.toInt() * 10).toString()
-            etvalue.text = "Ответ:  $value"
+            etvalue?.text = "Ответ:  $value"
 
         }
 
